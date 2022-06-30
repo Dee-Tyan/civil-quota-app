497e9b3335bd39a2cc250b5708607e8d685d013d
Dee-Tyan <debbietiyan@gmail.com>
2022-06-30 19:03:44 +0100

    header restyled

--- cq-app/src/components/Header.tsx
+++ cq-app/src/components/Header.tsx
@@ -7,9 +7,12 @@ function Header() {
     <div className='flex justify-between h-24 py-5 px-9 shadow-md space-y-3 space-x-8 bg-snow'>
         <img src={logo} />
         
-        <div className=' flex justify-end rounded'>
-        <h6 className='my-2.5'>Become a sponsor</h6>
-        <img src={profile} className='max-h-12 my-0.5' />
+        <div className=' flex justify-end'>
+        <div className='flex items-center rounded-lg mx-4 bg-gradient-to-r from-cyan-500 to-blue-500'>
+        <p className='text-snow px-2 text-sm' >Become a sponsor</p>
+        </div>
+        
+        <img src={profile} className='max-h-14 my-0.5' />
         </div>
     </div>
   )
