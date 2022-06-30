ac08752f51bd847ffe4535da7fdc64c908c2c723
Dee-Tyan <debbietiyan@gmail.com>
2022-06-30 18:16:44 +0100

    header component

--- cq-app/src/components/Header.tsx
+++ cq-app/src/components/Header.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import logo from "../assets/cd_logo.png"
+import profile from "../assets/profilebg.png"
 
 function Header() {
   return (
-    <div className='flex h-28 py-5 px-9 shadow-md'>
+    <div className='flex justify-between h-28 py-5 px-9 shadow-md space-y-3 space-x-8'>
         <img src={logo} />
         
+        <div className=' flex justify-end'>
+        <h4 className='my-3.5'>Become a sponsor</h4>
+        <img src={profile} className='max-h-12 my-0.5' />
+        </div>
     </div>
   )
 }
