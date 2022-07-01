95498db562ce6145075235396e75d5e827332d28
Dee-Tyan <debbietiyan@gmail.com>
2022-07-02 00:23:40 +0100

    profile template designed

--- cq-app/src/App.tsx
+++ cq-app/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Profile from './components/ProfileCard/Profile';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <Profile/>
     </div>
   );
 }
--- cq-app/src/components/ProfileCard/Profile.tsx
+++ cq-app/src/components/ProfileCard/Profile.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import profilecard from "../../assets/trainee.jpg"
 function Profile() {
   return (
-    <div>Profile</div>
+    <div className="w-44 text-xs">
+    <img src={profilecard} className="w-full rounded-lg" />
+    <p className="text-mblue"> Grace Achi</p>
+    <div className="block justify-center items-center ">
+        <span className="text-bgray">Age: 6 </span>
+        <p className="text-bgray">Location: Lagos</p>
+    </div>
+    
+    <p><span className="text-mblue">When I grow up I want to be like: Mosh Hamedani ⭐ </span></p>
+</div>
   )
 }
 
