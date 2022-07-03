0105b7c09647bde4da3f36ccd2b52d05dc62ee4c
Dee-Tyan <debbietiyan@gmail.com>
2022-07-03 08:02:14 +0100

    homepage design setup

--- cq-app/src/components/Pages/Home/Homepage.tsx
+++ cq-app/src/components/Pages/Home/Homepage.tsx
@@ -1,22 +1,33 @@
 import React, { ReactNode } from "react";
 import Profile from "../../ProfileCard/Profile";
-import profiledata from "../../../dummyprofiledata"
-import heroImg from "../../../assets/hero.jpg"
+import profiledata from "../../../dummyprofiledata";
+import heroImg from "../../../assets/hero.jpg";
 
 const Homepage = () => {
-    let renderProfile: ReactNode = profiledata.map(talent => {
-        return <Profile tname={talent.tname} age={talent.age} location={talent.location} mentor={talent.mentor}  />
-      })
+  let renderProfile: ReactNode = profiledata.map((talent) => {
+    return (
+      <Profile
+        tname={talent.tname}
+        age={talent.age}
+        location={talent.location}
+        mentor={talent.mentor}
+      />
+    );
+  });
   return (
     <div className="">
       <main>
-        <section className="w-full m-h-14">
-          <img src={heroImg}/>
+        <section className="w-full p-2 flex flex-col border-4 border-solid border-blue-200">
+          <img className="h-60" src={heroImg} />
+          <h1 className="mb-1 text-4xl">Empower the Next Generation</h1>
+          <p className="mt-0 text-sm ">
+            Join our Tech Talent community to sponsor the next generation of
+            innovators.
+          </p>
         </section>
 
-        <section className="flex flex-wrap gap-10">
-
-        {renderProfile}  
+        <section className="flex justify-center flex-wrap gap-10">
+          {renderProfile}
         </section>
       </main>
     </div>
